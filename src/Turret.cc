7e1a9ce2ed89d587ae184dc33216f203a9d0a47d
loger600 <loger600@tlvm-4-3-1.ad.liu.se>
2023-11-24 15:48:43 +0100

    Added aim Function to Turret

--- src/Turret.cc
+++ src/Turret.cc
@@ -1,5 +1,12 @@
 #include "../include/Turret.h"
 
+#include <cmath>
+
+Turret::Turret(const FileReader::Data & stats, bool isFriendly, sf::Vector2f pos, std::shared_ptr<sf::Time> frameDuration)
+: Dynamic(stats, isFriendly, pos, frameDuration), angle { 30 }
+{
+
+}
 
 void Turret::handleCollision(__attribute__((unused)) int,
                              __attribute__((unused)) int)
@@ -16,10 +23,20 @@ std::shared_ptr<Projectile> Turret::spawnProjectile(FileReader::Data& stats,
                                                     std::shared_ptr<sf::Time> frameDuration,
                                                     sf::Vector2f enemyPos)
 {
-    aim(enemyPos);
+    aim1(enemyPos);
 }
 
-void Turret::aim(sf::Vector2f enemyPos)
+void Turret::aim1(sf::Vector2f enemyPos)
 {
-    angle = 0;
+    float projectileSpeed = 5;
+    float g = 10;
+    angle = atan(pow(projectileSpeed, 2) - sqrt(pow(projectileSpeed, 4) - g * (g * pow(enemyPos.x, 2) + 2*enemyPos.y * pow(projectileSpeed, 2))) / (g * enemyPos.x));
+}
+
+void Turret::aim2(sf::Vector2f enemyPos)
+{
+    float projectileSpeed = 5;
+    float g = 10;
+    angle = 1/2 * acos(((ypos-enemyPos.y)/2 - g/pow(projectileSpeed, 2) * pow(xpos-enemyPos.x, 2)) / sqrt(pow(xpos-enemyPos.x, 2)
+     + pow(ypos-enemyPos.y, 2))) + 1/2 * atan((ypos-enemyPos.y) / (xpos-enemyPos.x));
 }
