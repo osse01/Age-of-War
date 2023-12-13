1d9ec08cd84f3c2b45223d428b9fe040c2463abf
Logan Eriksson <loger600@student.liu.se>
2023-12-13 01:57:35 +0100

    Cleaned Up h-file and Contructors for Button

--- src/Button.cc
+++ src/Button.cc
@@ -3,13 +3,33 @@
 #include <iostream>
 
 // Constructor for Button without Sprite
-Button::Button(const sf::Vector2f buttonSize, sf::Vector2f pos, sf::Color fillColor, sf::Color outlineColor, std::string buttonText, sf::Font& font)
 //This is a button with text
-: i {0}, clicked {false}, hasCooldown{false}, cooldown{0}, currentCooldown{0}, button { sf::RectangleShape(buttonSize) }, timer{}, text {std::make_shared<sf::Text>(buttonText, font)}, sprite {},
-  renderButton {std::make_shared<sf::RenderTexture>()}, buttonSprite {}, fillColor {sf::Color(112, 58, 7)}
+Button::Button( const sf::Vector2f buttonSize, sf::Vector2f pos,
+                sf::Color fillColor, sf::Color outlineColor, 
+                std::string buttonText, sf::Font& font )
+:
+    // Data Member Initialization List
+    //---------------------------------------------------------------
+    renderButton    { std::make_shared<sf::RenderTexture>()        }, 
+    text            { std::make_shared<sf::Text>(buttonText, font) }, 
+    button          { sf::RectangleShape(buttonSize)               },
+
+    timer           {},
+    buttonSprite    {},
+    sprite          {},
+
+    fillColor       { sf::Color(112, 58, 7) },
+
+    currentCooldown { 0 },
+    cooldown        { 0 },
+    i               { 0 },
+
+    hasCooldown     { false }, 
+    clicked         { false }
+    //---------------------------------------------------------------
 {
     // Set Button Appearance and Position
-    button.setFillColor(fillColor);
+    button.setFillColor (fillColor);
     button.setOutlineColor(outlineColor);
     button.setOutlineThickness(2.0f);
     button.setOrigin(button.getSize().x/2-2, button.getSize().y/2-2);
@@ -18,9 +38,9 @@ Button::Button(const sf::Vector2f buttonSize, sf::Vector2f pos, sf::Color fillCo
     // Add Text to Button
     text->setFillColor(sf::Color::Black);
     text->setCharacterSize( static_cast<int>(0.6*buttonSize.y) );
-    text->setOrigin(text->getGlobalBounds().width/2+2, text->getGlobalBounds().height/2 - 2);
+    text->setOrigin(text->getGlobalBounds().width/2+2, 
+                    text->getGlobalBounds().height/2 - 2);
     text->setPosition(button.getPosition());
-
 
     // Create Button
     renderButton->create(buttonSize.x+4, buttonSize.y+4);
@@ -36,12 +56,32 @@ Button::Button(const sf::Vector2f buttonSize, sf::Vector2f pos, sf::Color fillCo
 }
 
 // Constructor for Button with Sprite
-Button::Button(const sf::Vector2f buttonSize, sf::Vector2f pos, sf::Sprite& sprite, sf::Color fillColor, bool clicked, bool ability, float cd)
 // This is a button with a sprite
-: i {1}, clicked {clicked}, hasCooldown{ability}, cooldown{cd},  currentCooldown{cooldown},
-  button { sf::RectangleShape(buttonSize) }, timer{ sf::RectangleShape(button)}, text {}, sprite { sprite },
-  renderButton {std::make_shared<sf::RenderTexture>()}, buttonSprite {},
-  fillColor { fillColor }
+Button::Button( const sf::Vector2f buttonSize, sf::Vector2f pos,
+                sf::Sprite& sprite, sf::Color fillColor, 
+                bool clicked, bool ability, float cd )
+:
+    // Data Member Initialization List
+    //----------------------------------------------------------
+    renderButton    { std::make_shared<sf::RenderTexture>() },
+
+    text            {}, 
+
+    button          { sf::RectangleShape(buttonSize) },
+    timer           { sf::RectangleShape(button)     }, 
+
+    buttonSprite    {},
+
+    sprite          { sprite    },
+    fillColor       { fillColor },
+    currentCooldown { cooldown  },
+    cooldown        { cd        }, 
+
+    i               { 1 }, 
+
+    hasCooldown     { ability },
+    clicked         { clicked } 
+    //----------------------------------------------------------
 {
     renderButton->create(buttonSize.x + 4, buttonSize.y + 4);
 
@@ -79,10 +119,32 @@ Button::Button(const sf::Vector2f buttonSize, sf::Vector2f pos, sf::Sprite& spri
     buttonSprite.setPosition(pos);
 }
 
-Button::Button(const sf::Vector2f buttonSize, sf::Vector2f pos, sf::Color fillColor)
+// Constructor for Button with Sprite
 // This is a button with no sprite
-: i {2}, clicked {false}, hasCooldown{false}, cooldown{0},  currentCooldown{0}, button { sf::RectangleShape(buttonSize) }, timer{}, text {}, sprite {},
-  renderButton {std::make_shared<sf::RenderTexture>()}, buttonSprite {}, fillColor {fillColor}
+Button::Button( const sf::Vector2f buttonSize,
+                      sf::Vector2f pos, 
+                      sf::Color    fillColor)
+:
+    // Data Member Initialization Lists
+    //---------------------------------------------------------------
+
+    renderButton    { std::make_shared<sf::RenderTexture>() },
+    text            {},
+    button          { sf::RectangleShape(buttonSize) }, 
+
+    timer           {},
+    buttonSprite    {},
+    sprite          {},
+
+    fillColor       { fillColor },    
+
+    currentCooldown { 0 },
+    cooldown        { 0 }, 
+    i               { 2 }, 
+
+    hasCooldown     { false },
+    clicked         { false }
+    //---------------------------------------------------------------
 {
     renderButton->create(buttonSize.x+4, buttonSize.y+4);
 
